f8358d91402bcaa99f82f4c5fb7aad9d03c0073f
Kai Xu <xukai921110@gmail.com>
2016-10-08 22:30:36 +0100

    exp2 finished

--- fork/main.cpp
+++ fork/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 
 #include <ctime>
@@ -11,53 +12,42 @@
 int main() {
     using namespace std;
 
-    const int NUM = 100;    // number of processes to fork
-    const int DEPTH = 100;  // depth of the Fibonacci sequence
-
+    const int NUM = 10;     // number of processes to fork
+    const int DEPTH = 50;  // depth of the Fibonacci sequence
     int seq[DEPTH];         // an array to store the Fib seq
                             // this may need to be changed to a linked list based stack
-    clock_t start;          // variable to record the start time
+    clock_t start, end;     // variable to record the start and end time
+    pid_t pid;              // process id
 
     ofstream file ("/Users/kai/Turing/exps/aistats2017/fork/process.csv");
 
     for (int n = 0; n < DEPTH; n++) {
 
         // Compute the Fibonacci sequence
-        if (n == 0 or n == 1) {
-            seq[n] = 1;
-        } else {
-            seq[n] = seq[n - 1] + seq[n - 2];
-        }
-
-        double times[NUM];      // container to store elapsed times
+        if (n == 0 or n == 1) seq[n] = 1;
+        else                  seq[n] = seq[n - 1] + seq[n - 2];
 
         // Fork a batch of processes
-        pid_t pid;
         for (int ith = 0; ith < NUM; ith++) {
-            start = clock();
-            pid = fork();
+            start = clock();    // record the start time
+            pid = fork();       // fork the process
+            end = clock();      // record the end time
+
             if (pid) {
-                file << double(clock() - start) / CLOCKS_PER_SEC;
+                // Kill the forked processes
+                if (pid != -1) kill(pid, SIGKILL);
+                // Write the elapsed time to the CSV file
+                file << double(end - start) / CLOCKS_PER_SEC;
                 if (ith < NUM - 1) file << ';';
-                continue;
             } else {
-                break;
+                pause();
             }
         }
-        file << endl;
-
-        // Terminate the child processes immediately after creating
-        // Since we only want to measure the time creating them
-        if (pid) {
-            continue;
-        } else {
-            break;
-        }
-
+        file << endl;           // line break
     }
 
-    file.close();
 
+    file.close();               // close the CSV file
     return 0;
 }
 
